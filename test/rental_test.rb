e72346c88333fd99523712db2f9291cdd340bdb2
DanHutch <danhutch5280@gmail.com>
2018-09-05 14:57:58 -0600

    end of time, didn't finish iteration 4

--- test/rental_test.rb
+++ test/rental_test.rb
@@ -5,9 +5,17 @@ require 'minitest/pride'
 require './lib/boat'
 require './lib/dock'
 require 'pry'
+require './lib/rental'
+require "./lib/renter"
 
 class RentalTest < Minitest::Test
 
+  def test_it_exists
+    kayak_1 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    rental = Rental.new(kayak_1, patrick)
+    assert_instance_of(Rental, rental)
+  end
 
 
 end
