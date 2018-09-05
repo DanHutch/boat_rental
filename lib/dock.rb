622c9ac356ef2f90d31ac7173fd2894d919d941b
DanHutch <danhutch5280@gmail.com>
2018-09-05 14:29:54 -0600

    finished iteration 3

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,17 +1,43 @@
 ## dock.rb
 
 require 'pry'
+require './lib/boat'
 
-class Dock
+class Dock < Boat
+
   attr_reader :name,
-              :max_rental_time
+              :max_rental_time,
+              :revenue,
+              :rented_boats
 
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @revenue = 0
+    @rented_boats = []
   end
 
+  def rent(boat, renter)
+    @rented_boats << boat
+    boat.rented = true
+  end
 
+  def log_hour
+    @rented_boats.each do |boat|
+      if boat.hours_rented < max_rental_time
+        boat.add_hour
+      end
+    end
+  end
 
+  def return(boat)
+    boat.rented = false
+  end
+
+  def revenue
+    @rented_boats.map do |boat|
+      boat.hours_rented * boat.price_per_hour
+    end.sum
+  end
 
 end
---
+++ test/rental_test.rb
@@ -0,0 +1,13 @@
+## rental_test.rb
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/boat'
+require './lib/dock'
+require 'pry'
+
+class RentalTest < Minitest::Test
+
+
+
+end
